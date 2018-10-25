1e4160b6294252a7d451ba10a177435d41518808
Kamil <kamil.dziolak@gmail.com>
2018-10-25 16:50:37 +0200

    work on update user data in firebase

--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -28,23 +28,40 @@ export function addUserData (user) {
     }
 }
 
-export function editUserProfile (user, userID) {
+export function editUserProfile (user, userID, dataName) {
     return (dispatch, getState, {getFirebase, getFirestore} ) => {
 
-        const firestore = getFirestore();
-        const userEmail = getState().firebase.profile.email;
-        const userID = getState().firebase.auth.uid;
-        let docID = 0;
+        const editUserValue = (fb, form) => {
+            if(form !== fb && form !== 'Noname' ){
+                
+            } else if(form !== fb && form !== 0){
 
+            }
+        }
+
+        const firestore = getFirestore();
+        let docID = '';
         firestore.collection('users').where('userID','==' , userID).get().then((snap) => {
             snap.docs.forEach(el => {
                 docID = el.id;
             })
         }).then(() =>{
-            firestore.collection('users').doc(docID).update({
-                ...user
-            }).then(() => {
-                console.log('huuu ra')
+            let dataNames = ['valueName', 'valueAge', 'valueGrowth', 'valueGender', 'valueWeight'];
+            
+            firestore.collection('users').doc(docID).onSnapshot((el) => {
+                let {valueName, valueAge, valueGrowth, valueGender, valueWidth} = el.data();
+                let userDataFromFb = [valueName, valueAge, valueGrowth, valueGender, valueWidth];
+                let userDataFromForm = [user.valueName, user.valueAge, user.valueGrowth, user.valueGender, user.valueWidth];
+                userDataFromFb.forEach((userFromFb, i) => {
+                    userDataFromForm.forEach((userFromForm, j) =>{
+                        dataNames.forEach((dataName, k) => {
+                            if(i === j && k === i && j === k){
+                                editUserValue(userFromFb, userFromForm, dataName)
+                            }
+                        })
+                    })
+                })
+                
             })
         })
 
