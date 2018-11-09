14613682e605c19b0619084d436f637bd1bf3292
Kamil <kamil.dziolak@gmail.com>
2018-11-09 22:52:53 +0100

    add new "MeasurementsResults" component to project

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
+import { HashRouter, Route, Redirect} from 'react-router-dom'
+import { connect } from 'react-redux'
+
 import LoginPage from './components/container/LoginPage/LoginPage'
 import Menu from './components/container/Menu/Menu'
 import MainPage from './components/container/MainPage/MainPage'
-import { HashRouter, Route, Redirect} from 'react-router-dom'
 import AddWeightMeasurement from './components/container/AddWeightMeasurement/AddWeightMeasurement';
 import Profile from './components/container/Profile/Profile'
 import SignUp from './components/container/SignUp/SignUp'
-import { connect } from 'react-redux'
+import MeasurementsResults from './components/container/MeasurementsResults/MeasurementsResults';
 
 class App extends Component {
 
@@ -23,6 +25,7 @@ class App extends Component {
               <Menu /> : <Redirect to='/login'/>)} />
           <Route exact path='/' render={() => (!isEmpty ? <MainPage/> : <Redirect to='/login'/>)} />
           <Route exact path='/measurement/add' render={() => (!isEmpty ? <AddWeightMeasurement/> : <Redirect to='/login'/>)}/>
+          <Route exact path='/measurement/results' render={() => {!isEmpty ? <MeasurementsResults/> : <Redirect to='/login'/>}}/>
           <Route exact path='/profile' render={() => (!isEmpty ? <Profile/> : <Redirect to='/login'/>)} />
         </div>
       </HashRouter>
---
+++ src/components/container/MeasurementsResults/MeasurementsResults.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class MeasurementsResults extends Component {
+  render() {
+    return (
+      null
+    );
+  }
+}
+
+export default MeasurementsResults;
